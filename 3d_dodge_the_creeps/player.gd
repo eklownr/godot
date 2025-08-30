9ffabdcb842a4eabd747fe9334dbd012ad1eb119
eklownr <eklownr@gmail.com>
2025-08-30 15:47:59 +0200

    add mob

--- 3d_dodge_the_creeps/player.gd
+++ 3d_dodge_the_creeps/player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody3D
 
-@export var speed = 14 # move meter/sec
-@export var fall_acceleration = 75 # downward acceleration when in the air,meters / second squared
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
 
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("move_right"):
@@ -15,11 +16,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_back"):
-		direction.z += 1 # In 3D, the XZ is the ground plane
+		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 
-	if direction != Vector3.ZERO: # if moving
+	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
@@ -35,4 +36,3 @@ func _physics_process(delta: float) -> void:
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
-		
