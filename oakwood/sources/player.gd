8c304309b003fefc703ae12227a17c4f6fda694c
eklownr <eklownr@gmail.com>
2025-09-07 14:11:58 +0200

    add state

--- oakwood/sources/player.gd
+++ oakwood/sources/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var dubble_jump 
+var duck_state = false
+var powerup_state = false
 @onready var camera = $"../Camera"
 @onready var anim = $Anim
 
@@ -11,15 +13,29 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		state_jump_anim() # is player on it't way up or down?
 
-	# Handle jump.
+	# Handle jump and state		
 	if Input.is_action_just_pressed("action") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		dubble_jump = true
 	elif Input.is_action_just_pressed("action") and dubble_jump:
 		velocity.y = JUMP_VELOCITY
 		dubble_jump = false
-
+		#state_jump_anim() # dubble jump in the air
+		
+	# duck state
+	if Input.is_action_just_pressed("down"):
+		duck_state = true
+	elif Input.is_action_just_released("down"):
+		duck_state = false		
+	
+	# powerup state
+	if Input.is_action_just_pressed("action_z"):
+		powerup_state = true
+	elif Input.is_action_just_released("action_z"):
+		powerup_state = false
+	
 	# Get the input actions. right and left
 	var direction := Input.get_axis("left", "right")
 	if direction:
@@ -27,12 +43,30 @@ func _physics_process(delta: float) -> void:
 		camera.position.x = position.x
 		if direction > 0:
 			anim.flip_h = false
-			anim.play("run") # run right
+			if is_on_floor():
+				anim.play("run") # run right
 		elif  direction < 0:
-			anim.play("run") # run left
 			anim.flip_h = true
-	else:
+			if is_on_floor():
+				anim.play("run") # run left
+			
+	else: # Idle or duck
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$Anim.play("idle")
-		
+		if is_on_floor() and duck_state == false:
+			$Anim.play("idle")
+		elif duck_state and is_on_floor():
+			anim.play("duck")
+	powreup_anim() # check if powerup is playing			
 	move_and_slide()
+
+
+func state_jump_anim() -> void:
+	# play anim jump or falling
+	if velocity.y < 0: #player is moving up → play "jump"
+		anim.play("jump")
+	if velocity.y > 0: #player is moving down → play "fall"
+		anim.play()
+	 
+func powreup_anim() -> void:
+	if is_on_floor() and powerup_state == true and velocity.x == 0:
+		anim.play("power_up")
